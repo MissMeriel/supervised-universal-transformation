d3fd5512d0f1fcc23ee448d807237bf324185af1
Meriel Stein <meriel.k.stein@gmail.com>
2023-03-24 13:42:34 -0300

    Fixed grad being erased during loss calc

--- training/loss_functions.py
+++ training/loss_functions.py
@@ -28,9 +28,10 @@ class VIFLoss(nn.Module):
 
     def forward(self, recons, x):
         batch_size = recons.shape[0]
-        recons_loss = torch.zeros(batch_size).to(device=self.device)
+        recons_loss = torch.zeros(batch_size, requires_grad=True).to(device=self.device)
         for i in range(batch_size):
-            recons_loss[i] = (1 - self.torch_vif(recons[i], x[i])) / batch_size
+            temp = (1 - self.torch_vif(recons[i], x[i])) / batch_size
+            recons_loss[i] = temp
         recons_loss = torch.sum(recons_loss)
         pred_recons = self.base_model(recons)
         pred_orig = self.base_model(x)
@@ -118,3 +119,57 @@ def _get_sigmas(GT,P,win,mode='same',**kwargs):
     return filter2(GT*GT,win,mode)  - GT_sum_sq, \
            filter2(P*P,win,mode)  - P_sum_sq, \
            filter2(GT*P,win,mode) - GT_P_sum_mul
+
+
+################ OG LOSS FUNCTIONS
+
+def loss_fn_orig(recons, x, mu, log_var, kld_weight):
+    recons_loss = F.mse_loss(recons, x)
+    kld_loss = torch.mean(
+        -0.5 * torch.sum(1 + log_var - mu ** 2 - log_var.exp(), dim=1), dim=0
+    )
+    loss = recons_loss + kld_weight * kld_loss
+    return loss, recons_loss, kld_loss
+
+def loss_fn(recons, x, mu, log_var, kld_weight):
+    recons_loss = F.mse_loss(recons, x)
+    pred_recons = base_model(recons)
+    pred_orig = base_model(x)
+    prediction_loss = F.mse_loss(pred_orig, pred_recons)
+    kld_loss = torch.mean(
+        -0.5 * torch.sum(1 + log_var - mu ** 2 - log_var.exp(), dim=1), dim=0
+    )
+    loss = prediction_loss + recons_loss + kld_weight * kld_loss
+    print(f"{prediction_loss=:.4f}\t{recons_loss=:.4f}\t{(kld_weight * kld_loss)=:.4f}")
+    return loss, recons_loss, kld_loss
+
+################ THIS ONE DOESN'T WORK
+
+# from vif_utils import vif
+from skimage.metrics import structural_similarity as ssim
+from sewar.full_ref import vifp
+
+base_model = torch.load("../weights/model-DAVE2v3-lr1e4-100epoch-batch64-lossMSE-82Ksamples-INDUSTRIALandHIROCHIandUTAH-135x240-noiseflipblur.pt")
+def loss_fn_features(recons, x, mu, log_var, kld_weight):
+    # recons_loss = vif(recons, x)
+    # two identical images have SSIM score of 1
+    recons_ssim = recons.clone().detach().cpu().numpy() #.transpose()
+    x_ssim = x.clone().detach().cpu().numpy()
+    recons_loss = 0
+    for i in range(recons_ssim.shape[0]):
+        # recons_loss += (1 - ssim(recons_ssim[i], x_ssim[i],
+        #               data_range=x_ssim[i].max() - x_ssim[i].min(), win_size=3)) / recons_ssim.shape[0]
+        recons_loss += (1 - vifp(recons_ssim[i].transpose(1,2,0), x_ssim[i].transpose(1,2,0))) / recons_ssim.shape[0]
+    pred_recons = base_model(recons)
+    pred_orig = base_model(x)
+    prediction_loss = F.mse_loss(pred_orig, pred_recons)
+    # print(f"{prediction_loss=:.4f}   \t{recons_loss=:.4f}")
+    kld_loss = torch.mean(
+        -0.5 * torch.sum(1 + log_var - mu ** 2 - log_var.exp(), dim=1), dim=0
+    )
+    loss = prediction_loss + recons_loss + kld_weight * kld_loss
+    return loss, recons_loss, prediction_loss
+
+
+
+
--- training/train_lenscoder.py
+++ training/train_lenscoder.py
@@ -30,60 +30,12 @@ Path("models").mkdir(exist_ok=True, parents=True)
 Path(f"samples_{NAME}").mkdir(exist_ok=True, parents=True)
 Path(f"samples_{NAME}/iter").mkdir(exist_ok=True, parents=True)
 Path(f"samples_{NAME}/epoch").mkdir(exist_ok=True, parents=True)
-shutil.copy(__file__, NAME)
+shutil.copy(__file__, f"{NAME}")
 
-def loss_fn_orig(recons, x, mu, log_var, kld_weight):
-    recons_loss = F.mse_loss(recons, x)
-    kld_loss = torch.mean(
-        -0.5 * torch.sum(1 + log_var - mu ** 2 - log_var.exp(), dim=1), dim=0
-    )
-    loss = recons_loss + kld_weight * kld_loss
-    return loss, recons_loss, kld_loss
-
-
-# from vif_utils import vif
-from skimage.metrics import structural_similarity as ssim
-from sewar.full_ref import vifp
-
-base_model = torch.load("../weights/model-DAVE2v3-lr1e4-100epoch-batch64-lossMSE-82Ksamples-INDUSTRIALandHIROCHIandUTAH-135x240-noiseflipblur.pt")
-def loss_fn_features(recons, x, mu, log_var, kld_weight):
-    # recons_loss = vif(recons, x)
-    # two identical images have SSIM score of 1
-    recons_ssim = recons.clone().detach().cpu().numpy() #.transpose()
-    x_ssim = x.clone().detach().cpu().numpy()
-    recons_loss = 0
-    for i in range(recons_ssim.shape[0]):
-        # recons_loss += (1 - ssim(recons_ssim[i], x_ssim[i],
-        #               data_range=x_ssim[i].max() - x_ssim[i].min(), win_size=3)) / recons_ssim.shape[0]
-        recons_loss += (1 - vifp(recons_ssim[i].transpose(1,2,0), x_ssim[i].transpose(1,2,0))) / recons_ssim.shape[0]
-    pred_recons = base_model(recons)
-    pred_orig = base_model(x)
-    prediction_loss = F.mse_loss(pred_orig, pred_recons)
-    # print(f"{prediction_loss=:.4f}   \t{recons_loss=:.4f}")
-    kld_loss = torch.mean(
-        -0.5 * torch.sum(1 + log_var - mu ** 2 - log_var.exp(), dim=1), dim=0
-    )
-    loss = prediction_loss + recons_loss + kld_weight * kld_loss
-    return loss, recons_loss, prediction_loss
-
-
-
-def loss_fn(recons, x, mu, log_var, kld_weight):
-    recons_loss = F.mse_loss(recons, x)
-    pred_recons = base_model(recons)
-    pred_orig = base_model(x)
-    prediction_loss = F.mse_loss(pred_orig, pred_recons)
-    kld_loss = torch.mean(
-        -0.5 * torch.sum(1 + log_var - mu ** 2 - log_var.exp(), dim=1), dim=0
-    )
-    loss = prediction_loss + recons_loss + kld_weight * kld_loss
-    print(f"{prediction_loss=:.4f}\t{recons_loss=:.4f}\t{(kld_weight * kld_loss)=:.4f}")
-    return loss, recons_loss, kld_loss
 
 from skimage.metrics import structural_similarity as ssim
 def train(model, data_loader, num_epochs=300, device=torch.device("cpu"), sample_interval=200, lr=0.0005):
     optimizer = optim.Adam(model.parameters(), lr=lr)
-    # lr_scheduler = optim.lr_scheduler.ExponentialLR(optimizer, 0.95)
     # TODO: adjust gamma if it plateaus at some values
     lr_scheduler = optim.lr_scheduler.ExponentialLR(optimizer, gamma=0.95)
     kld_weight = 0.01 * data_loader.batch_size / len(data_loader.dataset)
@@ -107,9 +59,9 @@ def train(model, data_loader, num_epochs=300, device=torch.device("cpu"), sample
             x = Variable(x, requires_grad=True)
             y = Variable(y, requires_grad=False)
             recons, x, mu, log_var = model(x)
-            # recons_loss = torch.zeros(1).to(device)
 
-            loss, rloss, kloss = vifloss(recons, y) # loss_fn_features(recons, y, mu, log_var, kld_weight) #
+            loss, rloss, kloss = vifloss(recons, y) #
+            # loss, rloss, kloss = loss_fn_features(recons, y, mu, log_var, kld_weight) #
             loss.backward()
             losses.append(loss.item())
             optimizer.step()
@@ -122,8 +74,6 @@ def train(model, data_loader, num_epochs=300, device=torch.device("cpu"), sample
             batches_done = (epoch - 1) * len(data_loader) + i
 
         epoch_end_t = time.time()
-        # print(f"epoch time: {(epoch_end_t - epoch_start_t):.3f} seconds")
-        # print(f"total time: {(epoch_end_t - start_t):.3f} seconds")
         all_losses.append(losses)
         lr_scheduler.step()
         model = model.eval()
@@ -183,7 +133,7 @@ def main():
         np.random.seed(np.random.get_state()[1][0] + worker_id)
 
     trainloader = DataLoader(dataset, batch_size=BATCH_SIZE, shuffle=True, worker_init_fn=worker_init_fn)
-    print("time to load dataset: {}".format(time.time() - start_time))
+    print("time to load dataset: {:.3d}".format(time.time() - start_time))
 
     device = torch.device("cuda") if torch.cuda.is_available() else torch.device("cpu")
     print("Using device:", device)
