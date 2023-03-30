a2668734cbf641ba5d7a684742af4bd11b73548c
Meriel <meriel.k.stein@gmail.com>
2023-03-29 20:29:47 -0400

    add install script

---
+++ install.sh
@@ -0,0 +1,4 @@
+#!/usr/bin/bash
+
+pip3 install tensorboard kornia numpy black mypy matplotlib scipy scikit-image pandas opencv-python markupsafe==2.0.1
+pip3 install torch==1.9.0 torchvision torchaudio --extra-index-url https://download.pytorch.org/whl/cu111
