aabe9d17fac609c5742ae7275e34d082305d136f
Meriel <meriel.k.stein@gmail.com>
2023-08-07 00:32:18 -0400

    added missing pip packages to install.sh

--- install.sh
+++ install.sh
@@ -1,4 +1,10 @@
 #!/usr/bin/bash
 
-pip3 install tensorboard kornia numpy black mypy matplotlib scipy scikit-image pandas opencv-python markupsafe==2.0.1
-pip3 install torch==1.9.0 torchvision torchaudio --extra-index-url https://download.pytorch.org/whl/cu111
+python3.8 -m venv .venv-sut
+. .venv-sut/bin/activate
+# pip3 install tensorboard kornia numpy black mypy matplotlib scipy scikit-image pandas opencv-python markupsafe==2.0.1
+# pip3 install torch==2.0.1 torchvision torchaudio --extra-index-url https://download.pytorch.org/whl/cu111
+pip install -r portalreqs.txt
+pip install torch==1.9.0+cu111 torchvision==0.10.0+cu111 torchaudio==0.9.0 -f https://download.pytorch.org/whl/torch_stable.html
+pip install --upgrade torch torchvision
+pip install blurgenerator discorpy
